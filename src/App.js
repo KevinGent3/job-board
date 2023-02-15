045c4fff7ca616ce48df47458f0e8cc263d9d152
Kévin Gentilini <kevingentilini@macbook-pro.home>
2023-02-15 18:14:11 +0100

    CSS

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Components/Header";
+import Jobs from "./Components/Jobs";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Jobs></Jobs>
+    </div>
+  );
+}
+
+export default App;
